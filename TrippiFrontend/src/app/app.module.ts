f61173f5a4949e61b35ec403197784f419a677e3
PeterclaverKimuli <30950839+PeterclaverKimuli@users.noreply.github.com>
2021-10-21 21:01:54 -0400

    added auth0 sdk

--- TrippiFrontend/src/app/app.module.ts
+++ TrippiFrontend/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AuthModule } from '@auth0/auth0-angular';
 
 @NgModule({
   declarations: [
