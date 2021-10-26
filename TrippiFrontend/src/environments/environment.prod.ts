476438c7c710b378b50be63fea5922681316b0fd
nick-yelle <49138282+nick-yelle@users.noreply.github.com>
2021-10-26 19:59:26 -0400

    fixing environment variables

--- TrippiFrontend/src/environments/environment.prod.ts
+++ TrippiFrontend/src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  authDomain: 'dev-ow5ha2t0.us.auth0.com',
+  authClientId: 'G9Vc3asT1QL9PR5vfNtC0zIAIlJwNpon'
 };
--- TrippiFrontend/src/environments/environment.ts
+++ TrippiFrontend/src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   authDomain: 'dev-ow5ha2t0.us.auth0.com',
   authClientId: 'G9Vc3asT1QL9PR5vfNtC0zIAIlJwNpon'
 };
